ba69534574e1ab32722cc1c3d31953f422726089
rakarnotive <rachmadicoding@gmail.com>
2023-05-01 11:34:06 +0700

    Tugas 2 JS
    to Niomic Mentors, Fundamentals of JavaScript

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+let _examScore = 40;
+
+if (_examScore > 75) {
+  _resultEntrance = true
+}
+else {
+  _resultEntrance = false
+}
+  console.log(_resultEntrance);
+
+let _notification = "NIK must be filled!";
+  console.log(_notification);
+
+let _resultMatch = null;
+  console.log(_resultMatch);
+
+let _myAge;
+  console.log(_myAge);
